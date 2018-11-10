ce9c7fe18f178e40ba72e35e12f0a590652c1078
Aitor González Martínez <a.gonzalez.coin@gmail.com>
2018-11-10 15:45:52 +0100

    Modifiying parameters to suit STATERA

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb };
+unsigned char pchMessageStart[4] = { 0xd2, 0xc3, 0x34, 0x7b };
 
 CMessageHeader::CMessageHeader()
 {
